7662262e29e224f28d641fdbee28fb5bb096c102
root <root@t4.c.algebraic-hub-289022.internal>
2020-10-02 04:20:39 +0000

    Fix batchnorm

--- include/taso/ops.h
+++ include/taso/ops.h
@@ -176,10 +176,10 @@ struct Edge {
 
 struct EdgeCompare {
   bool operator()(const Edge& a, const Edge& b) const {
+    if (a.dstIdx != b.dstIdx) return a.dstIdx < b.dstIdx;
     if (!(a.srcOp == b.srcOp)) return a.srcOp < b.srcOp;
     if (!(a.dstOp == b.dstOp)) return a.dstOp < b.dstOp;
     if (a.srcIdx != b.srcIdx) return a.srcIdx < b.srcIdx;
-    if (a.dstIdx != b.dstIdx) return a.dstIdx < b.dstIdx;
     return false;
   };
 };
