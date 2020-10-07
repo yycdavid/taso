12d1262268fb0e6b0d9fee07aa281451462b52ef
Yichen Yang <yycdavid25@gmail.com>
2020-10-07 17:53:58 +0000

    New exp scripts

--- examples/resnet50.py
+++ examples/resnet50.py
@@ -1,6 +1,16 @@
 import taso as ts
 import onnx
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Main experiment script')
+    parser.add_argument('--result_file', type=str, default='resnet50_time.txt', metavar='S',
+        help='File to store times')
+    parser.add_argument('--alpha', type=float, default=1.0,
+        help='Threshold value')
+    parser.add_argument('--iter', type=int, default=100,
+        help='Number of iterations for backtracking search')
+    return parser.parse_args()
+
 def resnet_block(graph, input, strides, out_channels):
     w1 = graph.new_weight(dims=(out_channels,input.dim(1),1,1))
     t = graph.conv2d(input=input, weight=w1,
@@ -38,14 +48,17 @@ for i in range(3):
     t = resnet_block(graph, t, strides, 512)
     strides = (1,1)
 
-onnx_model = ts.export_onnx(graph)
-onnx.save(onnx_model, "resnet50_old.onnx")
 
 old_time = graph.run_time()
-#onnx.checker.check_model(onnx_model)
 
-new_graph = ts.optimize(graph, alpha=1.0, budget=1000)
+args = get_args()
+new_graph = ts.optimize(graph, alpha=args.alpha, budget=args.iter)
+
+#onnx.save(old_onnx, "old_bert.onnx")
 
 new_time = new_graph.run_time()
 print("Run time of original graph is: {}".format(old_time))
 print("Run time of optimized graph is: {}".format(new_time))
+
+with open(args.result_file, "a") as f:
+    f.write("{}\t{}\n".format(old_time, new_time))
--- examples/test_onnx.py
+++ examples/test_onnx.py
@@ -5,6 +5,12 @@ import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="Path to input ONNX file", required=True)
+parser.add_argument('--result_file', type=str, default='new_time.txt', metavar='S',
+    help='File to store times')
+parser.add_argument('--alpha', type=float, default=1.0,
+    help='Threshold value')
+parser.add_argument('--iter', type=int, default=100,
+    help='Number of iterations for backtracking search')
 
 args = parser.parse_args()
 
@@ -12,11 +18,14 @@ graph = taso.load_onnx(args.file)
 
 old_time = graph.run_time()
 
-new_graph = taso.optimize(graph, alpha = 1.0, budget = 100, print_subst = True)
+new_graph = taso.optimize(graph, alpha = args.alpha, budget = args.iter)
 
 new_time = new_graph.run_time()
 print("Run time of original graph is: {}".format(old_time))
 print("Run time of optimized graph is: {}".format(new_time))
 
-onnx_model = taso.export_onnx(new_graph)
-onnx.save(onnx_model, "{}.taso.onnx".format(args.file))
+with open(args.result_file, "a") as f:
+    f.write("{}\t{}\n".format(old_time, new_time))
+
+#onnx_model = taso.export_onnx(new_graph)
+#onnx.save(onnx_model, "{}.taso.onnx".format(args.file))
